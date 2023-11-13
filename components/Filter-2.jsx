1f59be6d35c81c464a3b77a449efab531540cce7
najmiddin <turgunpolatov3gmail.com>
2023-11-13 20:54:44 +0500

    commit

--- components/Filter-2.jsx
+++ components/Filter-2.jsx
@@ -131,8 +131,8 @@ const Filter = () => {
                                 <td className="px-2 py-4 admin_panel_td admin_panel-tih admin_panel_index ">{index + 1}</td>
                                 <td className="px-2 py-4 admin_panel_td">{t.newIsm}-sinf</td>
                                 <td className="px-2 py-4 admin_panel_td">{t.telephoneRaqami}</td>
-                                <td className="px-2 py-4 admin_panel_td">{t.school}</td>
-                                <td className='admin_panel_td'>{t.newSinfi}</td>
+                                <td className="px-2 py-4 admin_panel_td">{t.school}-maktab</td>
+                                <td className='admin_panel_td'>{t.newSinfi}-sinf</td>
                                 <td className="px-2 py-4 admin_panel_td">{t.MFY}</td>
                                 <td className="px-2 py-4 admin_panel_td">{new Date(t.createdAt).toLocaleString()}</td>
                                 <td className="px-2 py-4 admin_panel_td">{t.newDarsQoldirish}</td>
