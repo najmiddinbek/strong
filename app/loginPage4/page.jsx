fdfe439d0d86ffda53f3926bb9df42dad99613b2
najmiddin <turgunpolatov3gmail.com>
2023-11-16 13:45:37 +0500

    commit

--- app/loginPage4/page.jsx
+++ app/loginPage4/page.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import Link from "next/link"
 
 export default function Page() {
     const history = useRouter();
@@ -17,6 +18,9 @@ export default function Page() {
     return (
         <div>
             <div className="container">
+                <Link className='flex justify-end' href={"/adminMainPage"}>
+                    <button className='green py-3 px-10 text-white rounded-md'>Orqaga</button>
+                </Link>
                 <form className='flex flex-col items-center h-[100vh] justify-center ' onChange={handleInputChange}>
                     <input className='border py-3 px-2 w-[40%] rounded-md' type="text" placeholder='Parolingizni kiriting' />
                     <button className='green rounded-md text-white py-2 px-10 cursor-pointer mt-2'>Kirish</button>
